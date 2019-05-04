363258c1c2d87adf3504343349c10fcf609a8ccd
estandel <ethan.standel@wexinc.com>
2019-05-04 18:52:18 -0400

    Adds a joystick-state output to the Joystick component that calculates the magnitude in percent and direction in degrees for the largest available circle around the stick.

--- src/client/src/components/joystick/Joystick.tsx
+++ src/client/src/components/joystick/Joystick.tsx
@@ -1,79 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
-import Fab from '@material-ui/core/Fab';
+import { ReplaySubject } from "rxjs";
 
 import "./Joystick.scss";
 
-export interface Props {  };
+export interface Props {
+    onUpdateJoystickState: (state: JoystickState) => any;
+};
 export interface State {
-    handleBaseStyle: any;
-    handleStyle: any;
+    knobBaseStyle: any;
+    knobStyle: any;
     dragState: {
         x: number;
         y: number;
     };
 };
 
+export interface JoystickState {
+    directionalDegree: number;
+    powerPercentage: number;
+};
+
 export class Joystick extends React.Component<Props, State> {
+
+    public readonly joystickState$ = new ReplaySubject<JoystickState>();
 
     private readonly DEFAULT_DRAG_STATE = { x: 0, y: 0 };
     private readonly TRANSITION_TIME = 200; //ms
 
+    private readonly isPortraitView = window.innerWidth / window.innerHeight < 1;
+    private readonly selectiveRadius = (this.isPortraitView ? window.innerWidth : window.innerHeight) / 2;
+
+    private joystickElement: HTMLElement;
+
     public state = {
-        handleBaseStyle: {  },
-        handleStyle: {  },
+        knobBaseStyle: {  },
+        knobStyle: {  },
         dragState: this.DEFAULT_DRAG_STATE
     };
 
-    private updateTransition(transition: string) {
-        this.setState({
-            handleStyle: { ...this.state.handleStyle, transition },
-            dragState: this.DEFAULT_DRAG_STATE
-        });
+    public constructor(props: Props, state: State) {
+        super(props, state);
+
+        this.joystickState$.subscribe(this.props.onUpdateJoystickState);
     }
 
-    private resetDragState() {
-        this.updateTransition(`${this.TRANSITION_TIME}ms ease-in-out`);
-        setTimeout(() => this.updateTransition("initial"), this.TRANSITION_TIME);
-    }
-
-    private centerElementStyles(joystick: HTMLElement, elementToCenter: HTMLElement): any {
-        const handleTop = (joystick.clientHeight / 2) - (elementToCenter.clientHeight / 2);
-        const handleLeft = (joystick.clientWidth / 2) - (elementToCenter.clientWidth / 2);
-
-        return { top: `${handleTop}px`, left: `${handleLeft}px` };
+    public componentWillUnmount() {
+        this.joystickState$.complete();
     }
 
     public componentDidMount() {
-        const joystickElement = ReactDOM.findDOMNode(this) as HTMLElement;
-        const handleElement = joystickElement.querySelector(".handle") as HTMLElement;
-        const handleBaseElement = joystickElement.querySelector(".handleBase") as HTMLElement;
+        this.joystickElement = ReactDOM.findDOMNode(this) as HTMLElement;
+        this.centerAndSizeAbsoluteElements();
+    }
 
-        const handleStyle = this.centerElementStyles(joystickElement, handleElement);
-        const handleBaseStyle = this.centerElementStyles(joystickElement, handleBaseElement);
+    public onDrag(event: MouseEvent | TouchEvent) {
+        const clientX = "changedTouches" in event ? event.changedTouches[0].clientX : event.clientX;
+        const clientY = "changedTouches" in event ? event.changedTouches[0].clientY : event.clientY;
 
-        this.setState({ ...this.state, handleStyle, handleBaseStyle });
+        const x = (clientX - window.innerWidth / 2);
+        const y = (window.innerHeight / 2 - clientY);
+
+        this.updateJoystickState(x, y);
     }
 
     public render(): React.ReactNode {
         return (
             <div className={Joystick.name}>
 
-                <div className="handleBase" style={this.state.handleBaseStyle}></div>
+                <div className="knobBase" style={this.state.knobBaseStyle} />
 
-                <Draggable position={this.state.dragState} onStop={() => this.resetDragState()}>
-                    <div className="draggableElement" style={this.state.handleStyle}>
-                        <Fab className="handle"
-                             variant="round"
-                             size="large"
-                             color="secondary">
-                            <div></div>
-                        </Fab>
-                    </div>
+                <Draggable position={this.state.dragState}
+                           onDrag={event => this.onDrag(event as MouseEvent)}
+                           onStop={() => this.onStop()}>
+                    <div className="draggableElement" style={this.state.knobStyle} />
                 </Draggable>
 
             </div>
         );
+    }
+
+
+    private updateTransition(transition: string) {
+        this.setState({
+            knobStyle: { ...this.state.knobStyle, transition },
+            dragState: this.DEFAULT_DRAG_STATE
+        });
+    }
+
+    private onStop() {
+        this.updateTransition(`${this.TRANSITION_TIME}ms ease-in-out`);
+        setTimeout(() => this.updateTransition("initial"), this.TRANSITION_TIME);
+
+        this.updateJoystickState(0, 0);
+    }
+
+    private centerElementStyles(joystick: HTMLElement, centerableElementWidth: number): any {
+        const knobTop = (joystick.clientHeight / 2) - (centerableElementWidth / 2);
+        const knobLeft = (joystick.clientWidth / 2) - (centerableElementWidth / 2);
+
+        return { top: `${knobTop}px`, left: `${knobLeft}px` };
+    }
+
+    private centerAndSizeAbsoluteElements() {
+        const widthBase = this.isPortraitView ? window.innerWidth : window.innerHeight;
+        
+        const knobDiameter = widthBase * .5;
+        const knobStyle = {
+            width: `${knobDiameter}px`, height: `${knobDiameter}px`,
+            ...this.centerElementStyles(this.joystickElement, knobDiameter)
+        };
+
+        const knobBaseDiameter = widthBase * .8;
+        const knobBaseStyle = {
+            width: `${knobBaseDiameter}px`, height: `${knobBaseDiameter}px`,
+            ...this.centerElementStyles(this.joystickElement, knobBaseDiameter)
+        };
+
+        this.setState({ ...this.state, knobStyle, knobBaseStyle });
+    }
+
+    private calculatePowerPercentage(x: number, y: number) {
+        const hypotenuse = Math.sqrt(x**2 + y**2);
+
+        const powerPercentageMax = 100 * (hypotenuse / this.selectiveRadius);
+        const powerPercentage = powerPercentageMax > 100 ? 100 : Math.round(powerPercentageMax);
+
+        return powerPercentage;
+    }
+
+    private calculateDirectionalDegree(x: number, y: number) {
+        const inverseTanResult = Math.round(Math.atan2(y, x) * 180 / Math.PI);
+
+        if (inverseTanResult > 0) {
+            return inverseTanResult;
+        } else {
+            return 360 + inverseTanResult;
+        }
+    }
+
+    private updateJoystickState(x: number, y: number) {
+        this.joystickState$.next({
+            directionalDegree: this.calculateDirectionalDegree(x, y),
+            powerPercentage: this.calculatePowerPercentage(x, y)
+        });
     }
 }
--- src/client/src/components/root-element/RootElement.tsx
+++ src/client/src/components/root-element/RootElement.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core/styles';
 
-import { DefaultTheme } from '../../theme';
 import { Joystick } from '../joystick';
 import "./RootElement.scss";
 
 export const RootElement: React.FC = () => {
     return (
-        <MuiThemeProvider theme={DefaultTheme}>
-            <div className="rootContainer">
-                <Joystick />
-            </div>
-        </MuiThemeProvider>
+        <div className="rootContainer">
+            <Joystick onUpdateJoystickState={state => console.log(state)} />
+        </div>
     );
 }
