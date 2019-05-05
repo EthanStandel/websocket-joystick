1fe2993dc02f0bf8fc38460493d412d47078ba28
estandel <ethan.standel@wexinc.com>
2019-05-04 23:15:23 -0400

    Report changes of the Joystick to the web-socket.

--- src/client/src/components/root-element/RootElement.tsx
+++ src/client/src/components/root-element/RootElement.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 
-import { Joystick } from '../joystick';
-import "./RootElement.scss";
+import { Joystick, JoystickState } from '../joystick';
+import './RootElement.scss';
 
-export const RootElement: React.FC = () => {
-    return (
-        <div className="rootContainer">
-            <Joystick onUpdateJoystickState={state => console.log(state)} />
-        </div>
-    );
+export class RootElement extends React.Component {
+
+    public socket: WebSocket;
+    public readonly socketUrl = 'ws://localhost:8000/';
+
+    public constructor(props: any) {
+        super(props);
+
+        this.openSocket();
+    }
+
+    public sendUpdates(state: JoystickState) {
+        this.socket.send(JSON.stringify(state));
+    }
+    
+    public render(): React.ReactNode {
+        return (
+            <div className="rootContainer">
+                <Joystick onUpdateJoystickState={state => this.sendUpdates(state)} />
+            </div>
+        );
+    }
+
+    private openSocket() {
+        this.socket = new WebSocket(this.socketUrl);
+        this.socket.onopen = () => console.log("Open");
+        this.socket.onmessage = message => console.log("Message: ", message);
+        this.socket.onerror = error => {
+            console.log("Error: ", error);
+            this.socket.close();
+            this.socket.onclose = () => this.openSocket();
+        }
+    }
 }
