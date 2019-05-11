a9f1b2839c4291d571251b8351f26558a055b65e
estandel <ethan.standel@wexinc.com>
2019-05-11 18:32:05 -0400

    Adds routing to application.

--- client/src/components/root-element/RootElement.tsx
+++ client/src/components/root-element/RootElement.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 
-import { Joystick, JoystickState } from '../joystick';
-import { Subject, fromEvent } from 'rxjs';
-import { switchMapTo, tap, map } from 'rxjs/operators';
-import './RootElement.scss';
+import { JoystickPage } from '../../pages/joystick/JoystickPage';
 
 export class RootElement extends React.Component {
 
-    private readonly SOCKET_URL = 'ws://localhost:8000/';
-    private readonly joystickState$ = new Subject<JoystickState>();
-
-    public constructor(props: any) {
-        super(props);
-
-        const socket = new WebSocket(this.SOCKET_URL);
-
-        fromEvent(socket, "open").pipe(
-            tap(() => console.log("Socket open")),
-            switchMapTo(this.joystickState$),
-            map(state => JSON.stringify(state))
-        ).subscribe(stateString => socket.send(stateString));
-    }
-
     public render(): React.ReactNode {
         return (
-            <div className="rootContainer">
-                <Joystick joystickState$={this.joystickState$}>
-                    Drag 🕹 me!
-                </Joystick>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/joystick" component={JoystickPage} />
+                    <Redirect from="/" to="/joystick" />
+                </Switch>
+            </BrowserRouter>
         );
     }
+
 }
---
+++ client/src/pages/joystick/JoystickPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Joystick, JoystickState } from '../../components/joystick/Joystick';
+import { Subject, fromEvent } from 'rxjs';
+import { switchMapTo, tap, map } from 'rxjs/operators';
+import './JoystickPage.scss';
+
+export class JoystickPage extends React.Component {
+
+    private readonly SOCKET_URL = 'ws://localhost:8000/';
+    private readonly joystickState$ = new Subject<JoystickState>();
+
+    public constructor(props: any) {
+        super(props);
+
+        const socket = new WebSocket(this.SOCKET_URL);
+
+        fromEvent(socket, "open").pipe(
+            tap(() => console.log("Socket open")),
+            switchMapTo(this.joystickState$),
+            map(state => JSON.stringify(state))
+        ).subscribe(stateString => socket.send(stateString));
+    }
+
+    public render(): React.ReactNode {
+        return (
+            <div className="rootContainer">
+                <Joystick joystickState$={this.joystickState$}>
+                    Drag 🕹 me!
+                </Joystick>
+            </div>
+        );
+    }
+}
